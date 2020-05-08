7160f6d7023bcd0944713176b851e02791d5b3ac
Corentin MACE <corentin.mace@ynov.com>
2020-05-08 20:47:26 +0200

    UPDATE - TROP DE TRUC

--- bot.js
+++ bot.js
@@ -19,9 +19,25 @@ con.connect(err => {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`${PREFIX}help`)
 });
 
 client.on('message', message => {
+
+  if (message.content === PREFIX + "help") {
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`**HELP**`)
+    .setColor(0x4fba6f)
+    .addFields(
+      { name: `${PREFIX}help`, value: `Affiche la liste des commandes.`},
+      { name: `${PREFIX}island`, value: `Affiche le nom de votre ile si vous l'avez déjà rentré.`},
+      { name: `${PREFIX}island [name]`, value: `Defini le nom de vôtre île.`},
+      { name: `${PREFIX}prix [value]`, value: `Défini le prix du cours du navet pour vôtre pour la demi-journée correcpondante`},
+      { name: `${PREFIX}bourse`, value: `Affiche les prix de toutes les îles selon la demi-journée`},
+      { name: `${PREFIX}higher`, value: `Affiche le prix le plus haut de la journée correcpondante`},
+    )
+    message.channel.send(embed);
+  }
 
   let sql;
 
@@ -76,53 +92,128 @@ client.on('message', message => {
 
    if (message.content === PREFIX + "bourse") {
      let today = new Date().toISOString().slice(0, 10)
+     let date = new Date();
      console.log(today)
+     let time = date.getHours();
+     console.log(time);
      con.query(`SELECT * FROM bourse WHERE DATE(day) = '${today}'`, (err, rows) => {
        if(rows.length < 1) {
          message.channel.send("Aucun prix n'a été rentré aujourd'hui !");
        } else {
-         con.query(`SELECT discord_id, island, value, day FROM player, bourse WHERE player_id=player.id ORDER BY value`, (err, rows) => {
-           for (var i = 0; i < rows.length; i++) {
-             const embed = new Discord.MessageEmbed()
-             .setTitle(`${rows[i].island}`)
-             .setDescription(`<@${rows[i].discord_id}>`)
-             .addFields(
-               { name: 'Prix :', value: `${rows[i].value}`},
-               { name: 'Day :', value: `${rows[i].day}`},
-             )
-             message.channel.send(embed);
-             // message.channel.send(`Ile : ${rows[i].island} Prix : ${rows[i].value}`)
-             // console.log(rows[i]);
-           }
-         });
-       }
-     });
-   }
+       if(time >= 8 && time < 12) { //c'est le matin
+         message.channel.send("Voici la bourse pour le matin :");
+         con.query(`SELECT * FROM player, bourse WHERE TIMEDIFF(TIME(day), "08:00:00") < "04:00:00" AND player_id=player.id ORDER BY value`, (err, rows) => {
+                for (var i = 0; i < rows.length; i++) {
+                      const embed = new Discord.MessageEmbed()
+                      .setTitle(`${rows[i].island}`)
+                      .setDescription(`<@${rows[i].discord_id}>`)
+                      .setColor(0x4fba6f)
+                      .addFields(
+                        { name: 'Prix :', value: `${rows[i].value}`},
+                        { name: 'Day :', value: `${rows[i].day}`},
+                      )
+                      message.channel.send(embed);
+                }
+         })
+       } else if (time >= 12 && time <= 21) {
+         message.channel.send("Voici la bourse pour l'après-midi :");
+
+         con.query(`SELECT * FROM player, bourse WHERE TIMEDIFF(TIME(day), "08:00:00") > "04:00:00" AND player_id=player.id ORDER BY value`, (err, rows) => {
+                for (var i = 0; i < rows.length; i++) {
+                      const embed = new Discord.MessageEmbed()
+                      .setTitle(`${rows[i].island}`)
+                      .setDescription(`<@${rows[i].discord_id}>`)
+                      .setColor(0x4fba6f)
+                      .addFields(
+                        { name: 'Prix :', value: `${rows[i].value}`},
+                        { name: 'Day :', value: `${rows[i].day}`},
+                      )
+                      message.channel.send(embed);
+                }
+         })
+       } else {
+          message.channel.send("La boutique Nook est fermée mais voici la bourse du jour");
+          con.query(`SELECT discord_id, island, value, day FROM player, bourse WHERE player_id=player.id ORDER BY value`, (err, rows) => {
+            for (var i = 0; i < rows.length; i++) {
+              const embed = new Discord.MessageEmbed()
+              .setTitle(`${rows[i].island}`)
+              .setDescription(`<@${rows[i].discord_id}>`)
+              .setColor(0x4fba6f)
+              .addFields(
+                { name: 'Prix :', value: `${rows[i].value}`},
+                { name: 'Day :', value: `${rows[i].day}`},
+              )
+              message.channel.send(embed);
+              }
+          });
+        }
+      }
+    });
+  }
 
    if(message.content === PREFIX + "higher") {
      let today = new Date().toISOString().slice(0, 10)
+     let date = new Date();
      console.log(today)
+     let time = date.getHours();
+     console.log(time);
      con.query(`SELECT * FROM bourse WHERE DATE(day) = '${today}'`, (err, rows) => {
        if(rows.length < 1) {
          message.channel.send("Aucun prix n'a été rentré aujourd'hui !");
        } else {
-         con.query(`SELECT discord_id, island, value, day FROM player, bourse WHERE player_id=player.id ORDER BY value DESC LIMIT 1`, (err, rows) => {
-           for (var i = 0; i < rows.length; i++) {
-             const embed = new Discord.MessageEmbed()
-             .setTitle(`${rows[i].island}`)
-             .setDescription(`<@${rows[i].discord_id}>`)
-             .addFields(
-               { name: 'Prix :', value: `${rows[i].value}`},
-               { name: 'Day :', value: `${rows[i].day}`},
-             )
-             message.channel.send(embed);
-             // message.channel.send(`Ile : ${rows[i].island} Prix : ${rows[i].value}`)
-             // console.log(rows[i]);
-           }
-         });
-       }
-     });
+       if(time >= 8 && time < 12) { //c'est le matin
+         message.channel.send("Voici le prix le plus haut pour le matin :");
+         con.query(`SELECT * FROM player, bourse WHERE TIMEDIFF(TIME(day), "08:00:00") < "04:00:00" AND player_id=player.id ORDER BY value DESC LIMIT 1`, (err, rows) => {
+                for (var i = 0; i < rows.length; i++) {
+                      const embed = new Discord.MessageEmbed()
+                      .setTitle(`${rows[i].island}`)
+                      .setDescription(`<@${rows[i].discord_id}>`)
+                      .setColor(0x4fba6f)
+                      .addFields(
+                        { name: 'Prix :', value: `${rows[i].value}`},
+                        { name: 'Day :', value: `${rows[i].day}`},
+                      )
+                      message.channel.send(embed);
+                }
+         })
+       } else if (time >= 12 && time <= 21) {
+         message.channel.send("Voici le prix le plus haut pour l'après-midi :");
+
+         con.query(`SELECT * FROM player, bourse WHERE TIMEDIFF(TIME(day), "08:00:00") > "04:00:00" AND player_id=player.id ORDER BY value DESC LIMIT 1`, (err, rows) => {
+                for (var i = 0; i < rows.length; i++) {
+                      const embed = new Discord.MessageEmbed()
+                      .setTitle(`${rows[i].island}`)
+                      .setDescription(`<@${rows[i].discord_id}>`)
+                      .setColor(0x4fba6f)
+                      .addFields(
+                        { name: 'Prix :', value: `${rows[i].value}`},
+                        { name: 'Day :', value: `${rows[i].day}`},
+                      )
+                      message.channel.send(embed);
+                }
+         })
+       } else {
+          message.channel.send("La boutique Nook est fermée mais voici le prix le plus haut du jour");
+          con.query(`SELECT discord_id, island, value, day FROM player, bourse WHERE player_id=player.id ORDER BY value DESC LIMIT 1`, (err, rows) => {
+            for (var i = 0; i < rows.length; i++) {
+              const embed = new Discord.MessageEmbed()
+              .setTitle(`${rows[i].island}`)
+              .setDescription(`<@${rows[i].discord_id}>`)
+              .setColor(0x4fba6f)
+              .addFields(
+                { name: 'Prix :', value: `${rows[i].value}`},
+                { name: 'Day :', value: `${rows[i].day}`},
+              )
+              message.channel.send(embed);
+              }
+          });
+        }
+      }
+    });
    }
-});
+
+ });
+
+
 
 client.login(config.bot_token);
